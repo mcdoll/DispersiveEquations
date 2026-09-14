798546f6d2c2147a72e950ce57794009726a608c
Moritz Doll <moritz.doll@googlemail.com>
2026-09-15 09:56:56 +1000

    missing files

---
+++ DispersiveEquations/Bibliography.lean
@@ -0,0 +1,14 @@
+import VersoManual
+import DispersiveEquations.References
+
+open Verso.Genre Manual
+open DispersiveEquations.Refs
+
+#doc (Manual) "Bibliography" =>
+%%%
+htmlSplit := .never
+tag := "bibliography"
+%%%
+
+* {citehere Hormander1}[]
+* {citehere Tao2006}[]
